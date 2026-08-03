884e6189b6755674906863387cb8df008b60aedf
mingeon9411 <toube094@naver.com>
2026-08-03 10:15:33 +0900

    Initial commit

---
+++ src/main/java/com/example/dockerhubtest/DockerhubtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerhubtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerhubtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerhubtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dockerhubtest/DockerhubtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerhubtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerhubtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
